a27af4fc0cb2833ad06edceac4fe011504c011fc
Graeme Broomhall <graeme.broomhall@my.jcu.edu.au>
2016-09-21 14:10:35 +1000

    finish

--- DebuggingExercises/DebugTwelve1.java
+++ DebuggingExercises/DebugTwelve1.java
@@ -6,12 +6,13 @@ public class DebugTwelve1
      double num = -8.8, result;
      try
      {
-        if(num <= 0)
-           throw(new ArithmeticException());
+        if(num <= 0){
+            throw(new ArithmeticException());
+        }
         result = Math.log(num);
         System.out.println("Result is " + result);
      }
-     catch()
+     catch(ArithmeticException e)
      {
         System.out.println("Can't take logarithm for value of zero or lower");
      }
